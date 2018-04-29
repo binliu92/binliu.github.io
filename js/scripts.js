e5fffe07e98f04bbf6776750fa1f653de6597df6
binliu92 <binliu92@outlook.com>
2018-04-29 07:33:46 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var callback = function () {
+
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 2000);
+	});
+
+};
+
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
+
+function link_to_gmail(addr)
+{
+    location.href = "mailto:" + addr + "@gmail.com";
+}
